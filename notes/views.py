81c76a8cc9a8d358923a8a0e2acf15a07eff0fa1
Aikanysh Matyeva <aikanyshmatyeva@MacBook-Air-Elmira.local>
2022-10-12 14:56:25 +0600

    Added a feature for a logged user to be considered an author of the notes

--- notes/views.py
+++ notes/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import Http404
+from django.http import Http404, HttpResponse, HttpResponseRedirect
 from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
@@ -19,12 +19,18 @@ class NotesUpdateView(UpdateView):
     form_class = NotesForm
 
 
-class NotesCreateView(CreateView):
+class NotesCreateView(LoginRequiredMixin, CreateView):
     model = Notes
     success_url = "/smart/notes"
     form_class = NotesForm
+    login_url = "/admin"
 
-
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.user = self.request.user
+        self.object.save()
+        return HttpResponseRedirect(self.get_success_url())
+        
 
 class NotesListView(LoginRequiredMixin, ListView):
     model = Notes
