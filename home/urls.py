99a8640723c1dd4bffaefc5fd4ba393fc7700a92
Aikanysh Matyeva <aikanyshmatyeva@MacBook-Air-Elmira.local>
2022-10-12 14:30:41 +0600

    Created a web app for smart notes

---
+++ home/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('home', views.HomeView.as_view()),
+    path('authorized', views.AuthorizedView.as_view()),
+
+
+
+]
---
+++ notes/views.py
@@ -0,0 +1,46 @@
+from django.shortcuts import render
+from django.http import Http404
+from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
+
+from .forms import NotesForm
+from .models import Notes
+
+class NotesDeleteView(DeleteView):
+    model = Notes
+    success_url = "/smart/notes"
+    template_name = 'notes/notes_delete.html'
+
+    
+
+class NotesUpdateView(UpdateView):
+    model = Notes
+    success_url = "/smart/notes"
+    form_class = NotesForm
+
+
+class NotesCreateView(CreateView):
+    model = Notes
+    success_url = "/smart/notes"
+    form_class = NotesForm
+
+
+
+class NotesListView(ListView):
+    model = Notes
+    context_object_name = "notes"
+
+
+# def list(request):
+#     all_notes = Notes.objects.all()
+#     return render(request, 'notes/notes_list.html', {'notes': all_notes})
+
+class NotesDetailView(DetailView):
+    model = Notes
+    context_object_name = "note"
+
+# def detail(request, pk):
+#     try:
+#         note = Notes.objects.get(pk=pk)
+#     except Notes.DoesNotExist:
+#         raise Http404("Note doesn't exist")
+#     return render(request, 'notes/notes_detail.html', {'note': note})
